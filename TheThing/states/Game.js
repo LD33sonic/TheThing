9067d74283bf78d870612a357d19bdf801f05a58
Heraclio <ericworknode@gmail.com>
2015-08-25 00:41:48 -0600

    Files and logic updated

--- TheThing/states/Game.js
+++ TheThing/states/Game.js
@@ -11,15 +11,20 @@ Main.Game.prototype = {
 
 		this.player = this.add.sprite(model.player.startPos.x,model.player.startPos.y(this.world.height),'player');
 		this.physics.enable(this.player,Phaser.Physics.ARCADE);
+		this.player.height = model.player.height;
+		this.player.width = model.player.width;
 		this.player.body.collideWorldBounds = true;
 		this.player.body.bounce.y = model.player.bounce;
 		this.player.body.gravity.y = model.player.gravity;
+		this.player.body.drag.setTo(model.player.drag,0);
+		this.player.body.maxVelocity.setTo(model.player.maxSpeed,model.player.maxSpeed); //x,y
 
 		this.ground = this.add.sprite(0,model.world.ground.pos.y(this),'player');
 		this.physics.arcade.enable(this.ground);
 		this.ground.height = model.world.ground.height;
 		this.ground.width = model.world.ground.width(this);
 		this.ground.body.allowGravity = false;
+		this.ground.static = true;
 		this.ground.body.immovable = true;
 	},
 	update: function() {
@@ -42,13 +47,25 @@ var model = {
 	ui : {},
 	creatures : {},
 	player : {
+		height : 20,
+		width : 20,
 		startPos : {
 			x : 100,
 			y : (function(height){return height - 100})
 		},
 		gravity : 200,
 		bounce : 0.35,
-		speed : 20
+		jump : 100,
+		stompForce : 500,
+		airVelocityXBuffer : 4,
+		drag : 800, //pixels/second
+		acceleration : 1000, //pixels/second
+		maxSpeed : 200, //pixels/second
+		highJumpCounter : 0,
+		highJumpCounterLimit : 50,
+		ableToJump : false,
+		bouncing : false,
+		highJumpBaseBoost : 1.4
 	},
 	world : {
 		ground : {
@@ -61,7 +78,6 @@ var model = {
 		}
 	}
 }
-
 var controller = {
 	ui : {},
 	creatures : {},
@@ -69,11 +85,48 @@ var controller = {
 		collisions : (function(game){
 			game.physics.arcade.collide(game.player,game.ground);
 		}),
+		jump : (function(player){
+			player.body.velocity.y = -(model.player.jump + model.player.highJumpCounter * model.player.highJumpBaseBoost);
+			model.player.highJumpCounter = 0;
+			model.player.ableToJump = false;
+		}),
 		movement : (function(game){
-			if(game.cursors.right.isDown){game.player.body.velocity.x += model.player.speed}
-			if(game.cursors.left.isDown){game.player.body.velocity.x -= model.player.speed}
-			if(game.cursors.up.isDown){game.player.body.velocity.y -= model.player.speed}
-			if(game.cursors.down.isDown){game.player.body.velocity.y += model.player.speed}
+			if(game.player.body.touching.down){
+				model.player.ableToJump = true;
+				//Right
+				if(game.cursors.right.isDown){
+					game.player.body.acceleration.x = model.player.acceleration;
+				} else if(game.cursors.left.isDown){
+					game.player.body.acceleration.x = -model.player.acceleration;
+				} else {
+					game.player.body.acceleration.x = 0;
+				}
+				//Jump
+				if(game.cursors.up.isDown){
+				  game.player.body.velocity.y = -model.player.jump;
+				}
+				//Boost Jump
+				if(game.cursors.down.isDown){
+					model.player.highJumpCounter++;
+					if(model.player.highJumpCounter >= model.player.highJumpCounterLimit){
+						controller.player.jump(game.player);
+					}
+				}
+			} else {
+				//Stomp
+				if(Math.floor(game.player.body.velocity.y / 100) >= 0 && game.cursors.down.isDown){
+					game.player.body.velocity.y = game.player.body.velocity.y + (model.player.acceleration * model.player.stompForce);
+				}
+				//Buffered Velocity X
+				if(game.cursors.right.isDown){
+					game.player.body.acceleration.x = model.player.acceleration / model.player.airVelocityXBuffer;
+				} else if(game.cursors.left.isDown){
+					game.player.body.acceleration.x = -model.player.acceleration / model.player.airVelocityXBuffer;
+				} else {
+					game.player.body.acceleration.x = 0;
+				}
+			}
+			/*NORMALIZE BOUNCING WITH BOOL*/
 		})
 	},
 	world : {}
